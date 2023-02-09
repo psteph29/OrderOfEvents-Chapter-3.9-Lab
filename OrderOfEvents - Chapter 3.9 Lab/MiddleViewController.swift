30f38d8cbacf098976d83c56a8259847ee6797dd
Paige Stephenson <paigestephenson29@gmail.com>
2023-02-09 12:19:40 -0700

    Add files and storyboard for order of events lab

--- OrderOfEvents - Chapter 3.9 Lab/MiddleViewController.swift
+++ OrderOfEvents - Chapter 3.9 Lab/MiddleViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class MiddleViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nothingHasHappenedLabel: UILabel!
+    var eventNumber = 1
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addEvent(from: "viewDidLoad")
+        addEvent(from: "viewDidAppear")
+        addEvent(from: "viewWillDisappear")
+        addEvent(from: "viewDidDisappear")
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    func addEvent(from: String) {
+        if let existingText = nothingHasHappenedLabel.text {
+            nothingHasHappenedLabel.text = "\(existingText)\nEvent number \(eventNumber) was \(from)"
+            eventNumber += 1
+        }
+        
+        
+    }
+    
+    
     
 
     /*
